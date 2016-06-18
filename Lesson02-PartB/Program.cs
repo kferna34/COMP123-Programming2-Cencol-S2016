67600fd792507172320ebcdbfe55b0c18168f8c0
kferna34 <kferna34@my.centennialcollege.ca>
2016-06-18 11:51:28 -0400

    Did Lesson 1-Part B

---
+++ Lesson02-PartB/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Author : Krytia Fernandez
+ * Date Created: 05-13
+ * Date Modified: 05-13
+ * 
+ */
+namespace Lesson02_PartB
+{
+    /*
+     *this is the driver class for our program 
+     * 
+     * @class program 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUserName();
+        }
+        /*
+         * THis method reads the UserName from the console
+         * 
+         * @method GetUserName
+         * @return {string} UserName
+         */
+        public static string GetUserName()
+        {
+            //initialize var
+            string UserName = "";
+
+            OutputStringToConsole("Enter UserName:", false);
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("\n======================================\n");
+            Console.WriteLine("You Entered : " + UserName + "\n");
+
+            return UserName;
+        }
+        /*
+         * This method writes a string to the console. param allows the user to choose to ass a newLine character
+         * 
+         */
+        public static string OutputStringToConsole(string outputString, bool hasNewLine)
+        {
+            //refractored using the ternary operator
+            string suffixString = hasNewLine ? "\n" : "";
+            Console.WriteLine(outputString + suffixString);
+            return outputString;
+        }
+    }
+}
+
