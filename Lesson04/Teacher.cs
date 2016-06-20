125ccc395c0280407a28c47b65695efb67ae1512
kferna34 <kferna34@my.centennialcollege.ca>
2016-06-20 17:42:42 -0400

    did lesson 04 -- teacher class

---
+++ Lesson04/Teacher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson04
+{
+    /// <summary>
+    /// the teacher is derived from the person class
+    /// </summary>
+    class Teacher : Person
+    {
+        //PRIVATE VAR
+        private string _employeeID; 
+
+        //PUBLIC PROP
+        public string EmployeeID
+        {
+            get
+            {
+                return this._employeeID;
+            }
+            set
+            {
+                this._employeeID = value;
+            }
+        }
+        //CONSTRUCTORS
+        ///<summary>
+        ///this constructor method takes three param: name, age, employeeID
+        ///</summary>
+        public Teacher (string name , int age,string employeeID) : base(name,age)
+        {
+            this.EmployeeID = employeeID;
+        }
+        //PUBLIC METHOD
+        ///<summary>
+        ///this method enables the teaches behaviour for the Teacher class
+        ///</summary>
+        public void Teaches()
+        {
+            Console.WriteLine( this.Name + "teaches, ");
+        }
+    }
+}
