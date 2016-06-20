204bee12f06c15f1d79faa2e25acc6e8cbb0cc1c
kferna34 <kferna34@my.centennialcollege.ca>
2016-06-20 18:08:37 -0400

    finished lesson 04-- program class == lesson 04 DONE

--- Lesson04/Program.cs
+++ Lesson04/Program.cs
@@ -11,7 +11,65 @@ namespace Lesson04
         static void Main(string[] args)
         {
             //create instance of the student class
-            
+            Student tommy = new Student("Tommy", 20, "S123456789");
+            tommy.SaysHello();
+            Console.WriteLine();
+            tommy.Studies();
+            tommy.Courses.Add(new Course("COMP123", "Programming2"));
+            tommy.Courses.Add(new Course("COMP125", "Client-Sode Web Development"));
+            tommy.Courses.Add(new Course("COMP397", "Web GameProgramming2"));
+            tommy.ShowCourses();
+            Console.WriteLine();
+
+            //create instance of the teacher class
+            Teacher tom = new Teacher("Tom", 47, "T123456789");
+            tom.SaysHello();
+            Console.WriteLine();
+            tom.Teaches();
+
+            List<string> names = new List<string>();
+            names.Add("Tom");
+            names.Add("Mary");
+            names.Add("Rutvik");
+
+            int count = 0;
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++");
+            foreach (string name in names)
+            {
+                Console.WriteLine("Name #" + count + " is " + name);
+                count++;
+            }
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++");
+            List<Card> Deck = new List<Card>();
+            CreateDeck(Deck);
+        }
+        public static void CreateDeck(List<Card>deck)
+        {
+            string suit = "";
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                   case 0:
+                        suit = "hearts";
+                        break;
+                    case 1:
+                        suit = "clubs";
+                        break;
+                    case 2:
+                        suit = "diamonds";
+                        break;
+                    case 3:
+                        suit = "spades";
+                        break;
+                }
+                for (int face = 1; face < 14; face++)
+                {
+                    deck.Add(new Card(face, suit));
+                }
+            }
+        }
         }
     }
 }
